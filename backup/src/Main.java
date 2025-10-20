7c9b40663cf85b874d90ffa74559a2e532d88354
Daniel <daniel.urgiles.92@est.ucacue.edu.ec>
2025-10-20 18:35:56 -0500

    Agrega última actualización: 20/10/2025 06:32 PM -05

---
+++ backup/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, GitHub! Este es mi primer programa en Java en el repositorio.");
+        System.out.println("Tabla de multiplicar del 5:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%-5s %2d = %2d%n", "5 x " + i, i, 5 * i);
+        }
+        System.out.println();
+        System.out.println("Tabla de multiplicar del 3:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%-5s %2d = %2d%n", "3 x " + i, i, 3 * i);
+        }
+        System.out.println("Última actualización: 20/10/2025 06:32 PM -05");
+    }
+}
