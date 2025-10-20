a120bf890b2f5ea0693dd941697fc4262e19d86a
Daniel <daniel.urgiles.92@est.ucacue.edu.ec>
2025-10-20 17:44:36 -0500

    Tabla de multiplicar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("¡Hola, GitHub! Este es mi primer programa en Java en el repositorio.");
+        System.out.println("Tabla de multiplicar del 5:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
     }
 }
